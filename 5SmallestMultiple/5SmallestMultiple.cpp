a8113eb7b54350eac4f41d2ddd2e80f5cb7f3091
sajadadineh <sajadadineh@protonmail.com>
2020-04-06 20:28:14 +0430

    problem solving smalllest multiple

---
+++ 5SmallestMultiple/5SmallestMultiple.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long i = 1;
+	while (true){
+		int answerDivision = 0;
+		for(int j = 1; j < 21; j++){
+			if (i%j == 0){
+				answerDivision ++;
+			}else{
+				break;
+			}
+		}
+		if (answerDivision == 20){
+			cout<<i<<endl;
+			break;
+		}
+		i++;
+	}
+	return 0;
+}
